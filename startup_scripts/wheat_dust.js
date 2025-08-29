e135b1bc991e16afd4973d97549927587cc3b9bf
QJAG _ <QJAG10086@outlook.com>
2025-08-29 21:39:25 +0800

    fix textures

--- startup_scripts/wheat_dust.js
+++ startup_scripts/wheat_dust.js
@@ -3,7 +3,7 @@ StartupEvents.registry('item', event => {
   // The texture for this item has to be placed in kubejs/assets/kubejs/textures/item/test_item.png
   // If you want a custom item model, you can create one in Blockbench and put it in kubejs/assets/kubejs/models/item/test_item.json
   // If you want to specify a different texture location you can do that too, like this:
-  event.create('wheat_dust').texture('RegenerativeCraft:item/wheat_dust').maxStackSize(64)
+  event.create('wheat_dust').texture('regenerative_craft:item/wheat_dust').maxStackSize(64)
 
 })
 
