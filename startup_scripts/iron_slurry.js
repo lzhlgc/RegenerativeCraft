5cafb98eab64041fd88917bb398c22233f0d71e7
QJAG _ <QJAG10086@outlook.com>
2025-08-29 23:09:04 +0800

    Add Iron Slurry as a way to regenerate raw iron

---
+++ startup_scripts/iron_slurry.js
@@ -0,0 +1,9 @@
+// Listen to item registry event
+StartupEvents.registry('item', event => {
+  // The texture for this item has to be placed in kubejs/assets/kubejs/textures/item/test_item.png
+  // If you want a custom item model, you can create one in Blockbench and put it in kubejs/assets/kubejs/models/item/test_item.json
+  // If you want to specify a different texture location you can do that too, like this:
+  event.create('iron_slurry').texture('regenerative_craft:item/iron_slurry').maxStackSize(64)
+
+})
+
