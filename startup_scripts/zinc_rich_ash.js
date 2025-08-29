ca8732df3f2370ef9dfb21e34b3c0e12d6a0ffda
QJAG _ <QJAG10086@outlook.com>
2025-08-29 22:41:21 +0800

    Change some ids

--- startup_scripts/zinc_rich_ash.js
+++ startup_scripts/zinc_rich_ash.js
@@ -3,7 +3,7 @@ StartupEvents.registry('item', event => {
   // The texture for this item has to be placed in kubejs/assets/kubejs/textures/item/test_item.png
   // If you want a custom item model, you can create one in Blockbench and put it in kubejs/assets/kubejs/models/item/test_item.json
   // If you want to specify a different texture location you can do that too, like this:
-  event.create('zinc_rich_dust').texture('regenerative_craft:item/zinc_rich_dust').maxStackSize(64)
+  event.create('zinc_rich_ash').texture('regenerative_craft:item/zinc_rich_ash').maxStackSize(64)
 
 })
 
