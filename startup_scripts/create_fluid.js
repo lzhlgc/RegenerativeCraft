736f98aa5e2751344241ea9bae5e8d55d124a060
lzhlgc <103301085+lzhlgc@users.noreply.github.com>
2025-08-30 11:42:09 +0800

    Update create_fluid.js

--- startup_scripts/create_fluid.js
+++ startup_scripts/create_fluid.js
@@ -1,7 +1,7 @@
 StartupEvents.registry('fluid', event => {
-  event.create('common_ink').stillTexture('regenerative_craft:block/ink_still').flowingTexture('regenerative_craft:block/ink_flow').color(0x101011)
-  event.create('epic_ink').stillTexture('regenerative_craft:block/ink_still').flowingTexture('regenerative_craft:block/ink_flow').color(0xcd41c1)
-  event.create('legendary_ink').stillTexture('regenerative_craft:block/ink_still').flowingTexture('regenerative_craft:block/ink_flow').color(0xfcaf1c)
-  event.create('uncommon_ink').stillTexture('regenerative_craft:block/ink_still').flowingTexture('regenerative_craft:block/ink_flow').color(0x266b1f)
-  event.create('rare_ink').stillTexture('regenerative_craft:block/ink_still').flowingTexture('regenerative_craft:block/ink_flow').color(0x4da28f)
+  event.create('common_ink').stillTexture('regenerative_craft:fluid/ink_still').flowingTexture('regenerative_craft:fluid/ink_flow').color(0x101011)
+  event.create('epic_ink').stillTexture('regenerative_craft:fluid/ink_still').flowingTexture('regenerative_craft:fluid/ink_flow').color(0xcd41c1)
+  event.create('legendary_ink').stillTexture('regenerative_craft:fluid/ink_still').flowingTexture('regenerative_craft:fluid/ink_flow').color(0xfcaf1c)
+  event.create('uncommon_ink').stillTexture('regenerative_craft:fluid/ink_still').flowingTexture('regenerative_craft:fluid/ink_flow').color(0x266b1f)
+  event.create('rare_ink').stillTexture('regenerative_craft:fluid/ink_still').flowingTexture('regenerative_craft:fluid/ink_flow').color(0x4da28f)
 })
